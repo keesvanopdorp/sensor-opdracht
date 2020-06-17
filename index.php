ececc2a0288ee167e7b74351c3e698dcbb5e0097
kees <keesjrvanopdorp@live.nl>
2020-06-17 15:40:25 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <h2 class="navbar-brand">Klassen Monitor</h2>
+    </nav>
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-10 col-md-4 offset-md-0 offset-1">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+            <div class="col-10 col-md-4 offset-md-0 offset-1">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+            <div class="col-10 col-md-4 offset-md-0 offset-1">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row h25">
+            <div class="col-10 col-md-4 offset-md-0 offset-1">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+            <div class="col-10 col-md-4 offset-md-0 offset-1">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+            <div class="col-10 col-md-4 offset-md-0 offset-1 h-25">
+                <div class="shadow p-5 mb-5 bg-white rounded">
+                    <h2 class="text-center mt-3">21:43</h2>
+                    <h3 class="text-center my-1 text-muted">Do 9 juni 2020</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
